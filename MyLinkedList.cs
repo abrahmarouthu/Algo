22df7e45c5a17a24b84bf7b76b2553ff990446ce
Ashwin Brahmarouthu <bashwin.87@gmail.com>
2022-11-16 09:08:20 -0500

    Initial commit

---
+++ MyLinkedList.cs
@@ -0,0 +1,86 @@
+public class Node
+{
+    public int d;
+    public Node next;
+
+    public Node(int data)
+    {
+        d = data;
+        next = null;
+    }
+}
+
+public class LinkedListClass
+{
+    public Node head = null;
+
+    public void AddNode(int d)
+    {
+        var newNode = new Node(d);
+
+        if(head == null)
+            head = newNode;
+        else
+        {
+            var travelNode = head;
+            while(travelNode.next != null)
+                travelNode = travelNode.next;
+
+            travelNode.next = newNode;
+        }
+    }
+
+    public void AddNodeAtBeginning(int d)
+    {
+        var newNode = new Node(d);
+
+         if(head == null)
+            head = newNode;
+        else
+        {
+            //var temp = head;
+            newNode.next = head;
+            head = newNode;
+        }        
+    }
+
+    public void DeleteNode(int d)
+    {
+        if(head == null)
+            return;
+        else if(head.next == null)
+        {
+            if(head.d == d)
+                head = null;
+        }
+        else
+        {
+            var travelNode = head;
+            while(travelNode != null)
+            {
+                if(travelNode.next.d == d)
+                {
+                    travelNode.next = travelNode.next.next;
+                    return;
+                }
+                travelNode = travelNode.next;
+            }
+        }
+    }
+
+    public void Display()
+    {
+        if(head == null)
+            Console.WriteLine("LL is empty");
+        else
+        {
+            var travelNode = head;
+            while(travelNode != null)
+            {
+                Console.Write(travelNode.d + "->");
+                travelNode = travelNode.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// var s = new MyStack(3);
+//             s.Display();
+//             s.Push(10);
+//             s.Display();
+//             s.Push(20);
+//             s.Display();
+//             s.Push(5);
+//             s.Display();
+//             s.Pop();
+//             s.Display();
+//             s.Pop();
+//             s.Display();
+//             s.Pop();
+//             //s.Pop();
+//             s.Display();
+
+
+
+// var q = new MyQueue(5);
+//             q.Enqueue(5);
+//             q.Enqueue(10);
+//             q.Enqueue(15);
+//             q.Enqueue(20);
+//             q.Enqueue(25);
+//             q.Display();
+//             q.Enqueue(30);
+//             q.Display();
+//             q.Dequeue();
+//             q.Display();
+//             q.Enqueue(20);
+//             q.Display();
+//             q.Dequeue();
+//                         q.Display();
+//             q.Dequeue();
+//                         q.Display();
+//             q.Dequeue();
+//             q.Dequeue();
+//             q.Dequeue();
+//             q.Dequeue();
+//             q.Dequeue();
+//                         q.Dequeue();
+//             q.Dequeue();
+//             q.Display();
+
+var ll = new LinkedListClass();
+ll.Display();
+ll.AddNode(5);
+ll.AddNode(10);
+ll.Display();
+ll.AddNode(15);
+ll.AddNodeAtBeginning(2);
+ll.AddNode(20);
+ll.Display();
+ll.DeleteNode(10);
+ll.Display();
+Console.ReadLine();
